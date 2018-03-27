9ff3b9f74ad9e552e1478f5b78d4aa47c11965ab
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-03-27 13:51:48 -0600

    working on reading file and better errors

--- BeamAnalysis2.C
+++ BeamAnalysis2.C
@@ -2,12 +2,55 @@
 
 using namespace std;
 
-void BeamAnalysis2(){
-	const int nMeanBins = 5;
-	float meanBins[nMeanBins] = {2, 4, 6, 8, 12};
-	float adc12[nMeanBins] = {1399, 2834, 4147, 5441, 7563};
-	float sigma[nMeanBins] = {78, 114, 133, 157, 176};
-	float ex[nMeanBins]= {0,0,0,0,0};
+/* run 
+mean
+meanerror
+sigma 
+sigmaerror*/
+float runToEnergy(float run){
+	int r;
+	int s = (int) run;
+	switch (s){
+		case 558:
+			r=6;
+			break;
+		case 551:
+			r= 4;
+			break;
+		case 563:
+			r= 2;
+			break;
+		case 573:
+			r= 12;
+			break;
+		case 567:
+			r= 8;
+			break;
+		case 544:
+			r= 8;
+			break;
+		default:
+			r=-1;
+			break;
+	}
+	return r;
+}
+float* runToEnergy(queue<float> runs){
+	float *energies = new float[runs.size()];
+	for (unsigned i = 0; i < runs.size(); ++i)
+	{
+		energies[i] = runToEnergy(runs.front());
+		runs.pop();
+	}
+	return energies;
+}
+
+void trend(const int nMeanBins,float*meanBins, float* adc12, float* sigma, float* meanerror, float* sigmaerror){
+	float ex[nMeanBins];
+	for (int i = 0; i < nMeanBins; ++i)
+	{
+		ex[i] = 0;
+	}
 	TCanvas *canvas1 = new TCanvas();
 	TGraphErrors* mean = new TGraphErrors(nMeanBins,meanBins,adc12,ex,sigma); // how to set the uncertainty
 	TF1* lin = new TF1("lin","[1]*x+[0]",0,12);
@@ -37,4 +80,25 @@ void BeamAnalysis2(){
 	myText(.5,.32,kRed,Form("C2: %0.3f#pm %0.3f",nonLinearFactor,nonLinearError),.05);
 	myText(.5,.17,kRed,Form("Quad #chi^{2}/NDF: %0.2f",chi2/ndf),.05);
 
+}
+
+void BeamAnalysis2(){
+	int counter = 0;
+	ifstream inFile ("PbGl_data1.txt"); //txt file containing the data from BeamAnalysis1
+	const int LINES = 5;
+	queue<float> input[LINES];
+	string intemp;
+	stringstream ss;
+	for (int i = 0; i < LINES; ++i)
+	{
+		inFile>>intemp;
+		ss<<intemp;
+		getline(ss,intemp,',');
+		while(getline(ss,intemp,',')){
+			input[i].push(stof(intemp));
+			cout<<intemp<<endl;
+		}
+	}
+	float *energies = runToEnergy(input[0]);
+	//trend(input[1].size(),energies,queueToArray(input[1]),queueToArray(input[3]),queueToArray(input[2]),queueToArray(input[4]));
 }
