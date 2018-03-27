662e33caff2f59a53cd71eae4c27436c151e0bb5
Chase Smith <chsm5267@colorado.edu>
2018-03-27 17:30:42 -0600

    changed to use error on mean and error on sigma

--- BeamAnalysis3.C
+++ BeamAnalysis3.C
@@ -1,18 +1,19 @@
+#include "Frannamespace.C"
 using namespace std;
 
-void BeamAnalysis3(){
-	const int nMeanBins = 5;
-	float meanBins[nMeanBins] = {2, 4, 6, 8, 12};
-	float adc12[nMeanBins] = {1399, 2834, 4147, 5441, 7563};
-	float sigma[nMeanBins] = {78, 114, 133, 157, 176};
-	float ex[nMeanBins]= {0,0,0,0,0};
+void resolution(const int nMeanBins,float*meanBins, float* adc12, float* sigma, float* meanerror, float* sigmaerror){
+	float ex[nMeanBins];
+	for (int i = 0; i < nMeanBins; ++i)
+	{
+		ex[i] = 0;
+	}
 	TCanvas *canvas1 = new TCanvas();
 	float relativeE[nMeanBins];
 	float relativeU[nMeanBins];
 	for (int i = 0; i < nMeanBins; ++i)
 	{
 		relativeE[i] = sigma[i]/adc12[i];
-		relativeU[i]= errorDivide(sigma[i],0,adc12[i],sigma[i]);
+		relativeU[i]= errorDivide(sigma[i],sigmaerror[i],adc12[i],meanerror[i]);
 		cout<<relativeU[i]<<endl;
 	}
 	TF1* eF = new TF1("eF","TMath::Sqrt([0]*[0]/x+[1]*[1])",0,12);
@@ -24,12 +25,40 @@ void BeamAnalysis3(){
 	float errors[2];
 	errors[0] = eF->GetParError(0);
 	errors[1] = eF->GetParError(1);
-	ehist->Draw("AP");
+	//makeMarkerNice(&ehist, 1);
+	ehist->SetMarkerSize(2);
+	ehist->SetMinimum(0);
+	ehist->SetMaximum(0.1);
+	ehist->GetXaxis()->SetLimits(0,13);
+	ehist->Draw("AP*");
 	axisTitles(ehist,"Beam Energy GeV","#sigma/mean");
 	float chi = eF->GetChisquare();
 	int ndf = eF->GetNDF();
-	myText(.3,.75,kRed,Form("#chi^{2}:%0.2f NDF:%i",chi,ndf),.05);
-	myText(.3,.7,kRed,Form("#chi^{2}/NDF:%0.2f",chi/ndf),.05);
-	myText(.24,.85,kRed,Form("Stochastic: %0.6f#pm%0.6f ",eA,errors[0]),.05);
-	myText(.24,.8,kRed,Form("Constant: %0.6f#pm%0.6f",eB,errors[1]),.05);
+	myText(.3,.75,kRed,Form("#chi^{2}:%0.2f NDF:%i",chi,ndf),.05,0);
+	myText(.3,.7,kRed,Form("#chi^{2}/NDF:%0.2f",chi/ndf),.05,0);
+	myText(.24,.85,kRed,Form("Stochastic: %0.6f#pm%0.6f ",eA,errors[0]),.05,0);
+	myText(.24,.8,kRed,Form("Constant: %0.6f#pm%0.6f",eB,errors[1]),.05,0);
+}
+
+void BeamAnalysis3()
+{
+	int counter = 0;
+	ifstream inFile ("PbGl_data1.txt"); //txt file containing the data from BeamAnalysis1
+	const int LINES = 5;
+	queue<float> input[LINES];
+	string intemp;
+	stringstream ss;
+	for (int i = 0; i < LINES; ++i)
+	{
+		inFile>>intemp;
+		ss<<intemp;
+		getline(ss,intemp,',');
+		cout<<intemp<<":\n";
+		while(getline(ss,intemp,',')){
+			input[i].push(stof(intemp));
+			cout<<intemp<<endl;
+		}
+		ss.clear();
+	}
+	resolution(input[1].size(),queueToArray(input[0]),queueToArray(input[1]),queueToArray(input[3]),queueToArray(input[2]),queueToArray(input[4]));
 }
